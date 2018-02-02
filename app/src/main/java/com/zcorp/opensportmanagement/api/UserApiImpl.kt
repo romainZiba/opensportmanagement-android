fc6ad79ba5c9062d310b6d608fc89d4f40cd00c8
Romain Ziba <romain.ziba@docdoku.com>
2018-02-02 17:24:01 +0100

    Simulate a network call with Observable

--- app/src/main/java/com/zcorp/opensportmanagement/api/UserApiImpl.kt
+++ app/src/main/java/com/zcorp/opensportmanagement/api/UserApiImpl.kt
@@ -1,13 +1,32 @@
 package com.zcorp.opensportmanagement.api
 
 import io.reactivex.Observable
-import java.util.concurrent.TimeUnit
+
 
 /**
  * Created by romainz on 02/02/18.
  */
 class UserApiImpl: UserApi {
     override fun login(username: String, password: String): Observable<Boolean> {
-        return Observable.just(true).delay(2000, TimeUnit.MILLISECONDS)
+
+        val operationObservable: Observable<Boolean> = Observable.create({
+            it.onNext(longRunningOperation())
+            it.onComplete()
+        })
+        return operationObservable
     }
+
+    fun longRunningOperation(): Boolean {
+        try {
+            Thread.sleep(2000)
+        } catch (e: InterruptedException) {
+            // error
+        }
+
+        return true
+    }
+
+
+
+
 }
--- app/src/main/java/com/zcorp/opensportmanagement/screens/login/LoginPresenterImpl.kt
+++ app/src/main/java/com/zcorp/opensportmanagement/screens/login/LoginPresenterImpl.kt
@@ -1,20 +1,25 @@
 package com.zcorp.opensportmanagement.screens.login
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
 class LoginPresenterImpl(private val loginView: LoginView,
                          private val loginModel: LoginModel) : LoginPresenter {
 
     override fun validateCredentials(username: String, password: String) {
         loginView.showProgress()
         var observable = loginModel.login(username, password)
-        observable.subscribe{ aBoolean ->
-            run {
-                if (aBoolean) {
-                    loginView.navigateToHome()
-                } else {
-                    loginView.setPasswordError()
+        observable.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { aBoolean ->
+                    run {
+                        if (aBoolean) {
+                            loginView.navigateToHome()
+                        } else {
+                            loginView.setPasswordError()
+                        }
+                    }
                 }
-            }
-        }
     }
 
     override fun finish() { }
