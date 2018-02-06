0453568b66b516397ab2ac444f13709f00f6b5f4
Romain Ziba <romain.ziba@docdoku.com>
2018-02-06 22:32:56 +0100

    Use third-party library to handle the floating button

--- app/src/main/java/com/zcorp/opensportmanagement/screens/main/fragments/EventFragment/EventFragment.kt
+++ app/src/main/java/com/zcorp/opensportmanagement/screens/main/fragments/EventFragment/EventFragment.kt
@@ -3,17 +3,15 @@ package com.zcorp.opensportmanagement.screens.main.fragments.EventFragment
 import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
-import android.support.design.widget.FloatingActionButton
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
+import com.github.clans.fab.FloatingActionButton
 import com.zcorp.opensportmanagement.R
 import com.zcorp.opensportmanagement.api.EventApiImpl
-import kotlinx.android.synthetic.main.fragment_event_list.*
 
 /**
  * A fragment representing a list of Items.
@@ -36,10 +34,10 @@ class EventFragment : Fragment(), EventsView {
     private var presenter: EventsPresenter? = null
     private var fabExpanded: Boolean = true
 
-    private lateinit var fabEvent: LinearLayout
-    private lateinit var fabMatch: LinearLayout
-    private lateinit var fabDefaultEvent: LinearLayout
-    private lateinit var fabAddEvent: FloatingActionButton
+    //    private lateinit var fabEvent: LinearLayout
+//    private lateinit var fabMatch: LinearLayout
+//    private lateinit var fabDefaultEvent: LinearLayout
+//    private lateinit var fabAddEvent: FloatingActionButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,23 +53,13 @@ class EventFragment : Fragment(), EventsView {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val coordinatorLayout = inflater!!.inflate(R.layout.fragment_event_list, container, false) as CoordinatorLayout
-        fabEvent = coordinatorLayout.findViewById(R.id.layoutFabAddEvent)
-        fabMatch = coordinatorLayout.findViewById(R.id.layoutFabAddMatch)
-        fabDefaultEvent = coordinatorLayout.findViewById(R.id.layoutFabAddDefaultEvent)
-        fabAddEvent = coordinatorLayout.findViewById(R.id.fabAddEvent)
+
+//        fabAddEvent = coordinatorLayout.findViewById(R.id.menu)
         swipeRefreshLayout = coordinatorLayout.findViewById(R.id.swipeRefreshLayout)
         recyclerView = swipeRefreshLayout.findViewById(R.id.list)
         eventsNetworkError = coordinatorLayout.findViewById(R.id.eventsNetworkError)
         // Set the adapter
         recyclerView.adapter = EventRecyclerAdapter(presenter!!)
-        closeSubMenusFab()
-        fabEvent.setOnClickListener {
-            if (fabExpanded) {
-                closeSubMenusFab()
-            } else {
-                openSubMenusFab()
-            }
-        }
         return coordinatorLayout
     }
 
@@ -92,22 +80,6 @@ class EventFragment : Fragment(), EventsView {
         eventsNetworkError.visibility = View.INVISIBLE
         recyclerView.adapter.notifyDataSetChanged()
         swipeRefreshLayout.isRefreshing = false
-    }
-
-    //closes FAB submenus
-    private fun closeSubMenusFab() {
-        fabMatch.visibility = View.INVISIBLE
-        fabDefaultEvent.visibility = View.INVISIBLE
-        fabAddEvent.setImageResource(R.drawable.ic_add_black_24dp)
-        fabExpanded = false
-    }
-
-    //Opens FAB submenus
-    private fun openSubMenusFab() {
-        fabMatch.visibility = View.VISIBLE
-        fabDefaultEvent.visibility = View.VISIBLE
-        fabAddEvent.setImageResource(R.drawable.ic_clear_black_24dp)
-        fabExpanded = true
     }
 
     companion object {
