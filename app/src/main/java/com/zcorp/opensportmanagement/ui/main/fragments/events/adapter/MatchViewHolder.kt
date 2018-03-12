61fcc58b598eae8dc8baa16cf974302f3e969f62
Romain Ziba <romain.ziba@docdoku.com>
2018-03-12 23:32:49 +0100

    ui for match details

--- app/src/main/java/com/zcorp/opensportmanagement/ui/main/fragments/events/adapter/MatchViewHolder.kt
+++ app/src/main/java/com/zcorp/opensportmanagement/ui/main/fragments/events/adapter/MatchViewHolder.kt
@@ -3,7 +3,7 @@ package com.zcorp.opensportmanagement.ui.main.fragments.events.adapter
 import android.graphics.drawable.Drawable
 import android.view.View
 import com.zcorp.opensportmanagement.ui.main.fragments.events.IEventsPresenter
-import kotlinx.android.synthetic.main.match_description_layout_black_text_color.view.*
+import kotlinx.android.synthetic.main.match_description_layout.view.*
 
 /**
  * Created by romainz on 03/02/18.
